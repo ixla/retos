5accf95cc82832c5e70ede6e326415d73caa5b98
ixla <sila.fernandez@gmail.com>
2013-07-30 23:08:37 +0200

    guarda retos en bbdd

--- addedit.php
+++ addedit.php
@@ -48,12 +48,6 @@ if ($canDelete && $reto_id) {
     $titleBlock->addCrumbDelete('delete link', $canDelete, $msg);
 }
 $titleBlock->show();
-$prj = new CProject();
-$projects = $prj->getAllowedProjects($AppUI->user_id);
-foreach ($projects as $project_id => $project_info) {
-    $projects[$project_id] = $project_info['project_name'];
-}
-$projects = arrayMerge(array('0' => $AppUI->_('All', UI_OUTPUT_JS)), $projects);
 ?>
 <script src="./modules/retos/addedit.js" type="text/javascript"></script>
 
@@ -69,73 +63,9 @@ $projects = arrayMerge(array('0' => $AppUI->_('All', UI_OUTPUT_JS)), $projects);
             </td>
         </tr>
         <tr>
-            <td align="right"><?php echo $AppUI->_('Impacto'); ?>:</td>
+            <td align="right"><?php echo $AppUI->_('Reto Sigla'); ?>:</td>
             <td>
-                <?php
-                echo arraySelect($retoImpact, 'reto_impacto', 'size="1" class="text"', $reto->reto_impact);
-                ?>
-            </td>
-        </tr>
-        <tr>
-            <td align="right"><?php echo $AppUI->_('Reto Priority'); ?>:</td>
-            <td>
-                <input type="text" class="text" size="5" name="reto_priority" value="<?php echo $reto->reto_priority; ?>" maxlength="3">
-            </td>
-        </tr>
-        <tr>
-            <td align="right">&nbsp;&nbsp;<?php echo $AppUI->_('Status'); ?>:</td>
-            <td>
-                <?php
-                echo arraySelect($retoStatus, 'reto_status', 'size="1" class="text"', $reto->reto_status);
-                ?>
-            </td>
-        </tr>
-        <tr>
-            <td align="right">&nbsp;&nbsp;<?php echo $AppUI->_('Owner'); ?>:</td>
-            <td>
-                <?php
-                echo arraySelect($users, 'reto_owner', 'size="1" class="text"', ($reto->reto_owner ? $reto->reto_owner : $AppUI->user_id));
-                ?>
-            </td>
-        </tr>
-        <tr>
-            <td align="right">&nbsp;&nbsp;<?php echo $AppUI->_('Project'); ?>:</td>
-            <td>
-                <?php
-                echo arraySelect($projects, 'reto_project', 'size="1" class="text" onChange="updateMedidas();"', $reto->reto_project);
-                ?>
-            </td>
-        </tr>
-        <tr>
-            <td align="right">&nbsp;&nbsp;<?php echo $AppUI->_('Medida'); ?>:</td>
-            <td>
-                <?php
-                $medidas = array();
-                if ($reto->reto_project) {
-                    $medidaList = $reto->getMedidas($AppUI, $reto->reto_project);
-                    foreach ($medidaList as $id => $values) {
-                        $medidas[$id] = $values['medida_name'];
-                    }
-                }
-                $medidas = arrayMerge(array('0' => $AppUI->_('Not Specified', UI_OUTPUT_JS)), $medidas);
-                echo arraySelect($medidas, 'new_medida', 'size="1" class="text"', $reto->reto_medida);
-                ?>
-            </td>
-        </tr>
-        <tr>
-            <td align="right">&nbsp;&nbsp;<?php echo $AppUI->_('Programa'); ?>:</td>
-            <td>
-                <?php
-                $programas = array();
-                if ($reto->reto_project) {
-                    $programaList = $reto->getProgramas($AppUI, $reto->reto_project);
-                    foreach ($programaList as $id => $values) {
-                        $programas[$id] = $values['programa_name'];
-                    }
-                }
-                $programas = arrayMerge(array('0' => $AppUI->_('Not Specified', UI_OUTPUT_JS)), $programas);
-                echo arraySelect($medidas, 'new_programa', 'size="1" class="text"', $reto->reto_programa);
-                ?>
+                <input type="text" class="text" size="5" name="reto_sigla" value="<?php echo $reto->reto_sigla; ?>" maxlength="3">
             </td>
         </tr>
         <tr>
@@ -146,10 +76,10 @@ $projects = arrayMerge(array('0' => $AppUI->_('All', UI_OUTPUT_JS)), $projects);
         </tr>
         <tr>
             <td>
-                <input class="text" type="submit" value="back">
+                <input class="text" type="submit" value="Volver">
             </td>
             <td align="right">
-                <input class="text" type="submit" value="submit">
+                <input class="text" type="submit" value="Enviar">
             </td>
         </tr>
     </table>
